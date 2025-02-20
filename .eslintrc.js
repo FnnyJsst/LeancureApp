33a5343aeb928e91952ca28c6667cdd9837de180
FnnyJsst <fanny.jousset@gmail.com>
2025-02-20 14:08:59 +0100

    chore: ajout configuration ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    // Vos règles personnalisées ici
+    'prettier/prettier': 0,
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+  },
+}; 
