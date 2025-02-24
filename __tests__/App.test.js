dcacbf4a1149f1e3f24e328593ce4aaa9abe76d4
FnnyJsst <fanny.jousset@gmail.com>
2025-02-24 13:57:15 +0100

    test: tests done in webview settings

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -107,3 +107,12 @@
 //   //   expect(getByTestId('webview-screen')).toBeTruthy();
 //   // });
 // });
+
+import { render } from '@testing-library/react-native';
+import App from '../App';
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
+});
