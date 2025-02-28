4de6d8174a7425b06ce4a565f79078b017ff14db
FnnyJsst <fanny.jousset@gmail.com>
2025-02-28 08:20:30 +0100

    chore: updated dependencies

--- jest.setup.js
+++ jest.setup.js
@@ -7,9 +7,6 @@ import '@testing-library/jest-native/extend-expect';
 import 'react-native-gesture-handler/jestSetup';
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-jest.mock('@react-native-async-storage/async-storage', () =>
-  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
-);
 
 
 jest.mock('expo-secure-store', () => ({
--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  assets: ['./assets/fonts'],
+  project: {
+    android: {
+      packageName: "com.leancure.app"
+    }
+  },
+  assets: ['./assets/fonts']
 };
